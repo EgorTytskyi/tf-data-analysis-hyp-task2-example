fb445c4ce650a6c8ab5ffd700d05a6f645a6fb0f
EgorTytskyi <129987284+EgorTytskyi@users.noreply.github.com>
2023-04-13 23:15:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,30 +1,48 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
 import random
+from scipy.stats import ks_2samp
+
+
+
 
 chat_id = 628156322 # Ваш chat ID, не меняйте название переменной
 
 def solution(data) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    alpha = 0.04
+# чтение данных из файлов
+data_hist = np.loadtxt('historical_data.csv')
+data1 = np.loadtxt('modified_data_of_type_1.csv')
+data2 = np.loadtxt('modified_data_of_type_2.csv')
+data3 = np.loadtxt('modified_data_of_type_3.csv')
 
-# Пороговое значение
-    threshold = 300
+# вычисление эмпирических функций распределения
+ecdf_hist = np.arange(1, len(data_hist) + 1) / len(data_hist)
+ecdf1 = np.arange(1, len(data1) + 1) / len(data1)
+ecdf2 = np.arange(1, len(data2) + 1) / len(data2)
+ecdf3 = np.arange(1, len(data3) + 1) / len(data3)
 
-# Расчет среднего значения и стандартного отклонения
-    mean = sum(data) / len(data)
-    std = stats.tstd(data)
+# вычисление статистики критерия Колмогорова-Смирнова
+ks_stat1, p_value1 = ks_2samp(data_hist, data1)
+ks_stat2, p_value2 = ks_2samp(data_hist, data2)
+ks_stat3, p_value3 = ks_2samp(data_hist, data3)
 
-# Расчет t-статистики
-    n = len(data)
-    t_stat = (mean - threshold) / (std / (n**0.5))
+# сравнение полученной статистики с критическим значением
+n = len(data_hist)
+alpha = 0.04
+critical_value = np.sqrt(-0.5 * np.log(alpha / 2)) / np.sqrt(n)
+if ks_stat1 > critical_value or ks_stat2 > critical_value or ks_stat3 > critical_value:
+    print(True)
+else:
+    print(False)
 
-# Расчет критического значения
-    crit_val = stats.t.ppf(alpha, df=n-1)
 
-# Принятие решения
-    random_bool = random.choice([True, False])
 
-# Выводим случайное значение
-# Принятие решения
+
+
+
+
+
+
+
+
     return random_bool
